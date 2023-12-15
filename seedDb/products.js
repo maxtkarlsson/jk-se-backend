0f27466c1833d14456f1595cddf7fb8527ba2541
maxtkarlsson <113439377+maxtkarlsson@users.noreply.github.com>
2023-12-15 15:19:34 +0100

    (feat): Add more products to seed file

--- seedDb/products.js
+++ seedDb/products.js
@@ -1,8 +1,310 @@
 exports.mockProductData = [
   {
-    name: "Ring",
-    description: "This is a ring of gold.",
+    name: "Cosmic Knots",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     category: "ring",
+    price: 2000,
+    imageUrls: {
+      productImages: {
+        small: [
+          "../assets/productImages/81x57.png",
+          "../assets/productImages/81x57.png",
+          "../assets/productImages/81x57.png",
+        ],
+        medium: [
+          "../assets/productImages/464x323.png",
+          "../assets/productImages/464x323.png",
+          "../assets/productImages/464x323.png",
+        ],
+        large: [
+          "../assets/productImages/624x415.png",
+          "../assets/productImages/624x415.png",
+          "../assets/productImages/624x415.png",
+        ],
+      },
+      modelImages: {
+        small: [
+          "../assets/productImages/81x57.png",
+          "../assets/productImages/81x57.png",
+          "../assets/productImages/81x57.png",
+        ],
+        medium: [
+          "../assets/productImages/464x323.png",
+          "../assets/productImages/464x323.png",
+          "../assets/productImages/464x323.png",
+        ],
+        large: [
+          "../assets/productImages/624x415.png",
+          "../assets/productImages/624x415.png",
+          "../assets/productImages/624x415.png",
+        ],
+      },
+    },
+  },
+  {
+    name: "Nebula Nectar Stone",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    category: "necklace",
+    price: 13000,
+    imageUrls: {
+      productImages: {
+        small: [
+          "../assets/productImages/81x57.png",
+          "../assets/productImages/81x57.png",
+          "../assets/productImages/81x57.png",
+        ],
+        medium: [
+          "../assets/productImages/464x323.png",
+          "../assets/productImages/464x323.png",
+          "../assets/productImages/464x323.png",
+        ],
+        large: [
+          "../assets/productImages/624x415.png",
+          "../assets/productImages/624x415.png",
+          "../assets/productImages/624x415.png",
+        ],
+      },
+      modelImages: {
+        small: [
+          "../assets/productImages/81x57.png",
+          "../assets/productImages/81x57.png",
+          "../assets/productImages/81x57.png",
+        ],
+        medium: [
+          "../assets/productImages/464x323.png",
+          "../assets/productImages/464x323.png",
+          "../assets/productImages/464x323.png",
+        ],
+        large: [
+          "../assets/productImages/624x415.png",
+          "../assets/productImages/624x415.png",
+          "../assets/productImages/624x415.png",
+        ],
+      },
+    },
+  },
+  {
+    name: "Druzy Agate",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    category: "earring",
+    price: 10000,
+    imageUrls: {
+      productImages: {
+        small: [
+          "../assets/productImages/81x57.png",
+          "../assets/productImages/81x57.png",
+          "../assets/productImages/81x57.png",
+        ],
+        medium: [
+          "../assets/productImages/464x323.png",
+          "../assets/productImages/464x323.png",
+          "../assets/productImages/464x323.png",
+        ],
+        large: [
+          "../assets/productImages/624x415.png",
+          "../assets/productImages/624x415.png",
+          "../assets/productImages/624x415.png",
+        ],
+      },
+      modelImages: {
+        small: [
+          "../assets/productImages/81x57.png",
+          "../assets/productImages/81x57.png",
+          "../assets/productImages/81x57.png",
+        ],
+        medium: [
+          "../assets/productImages/464x323.png",
+          "../assets/productImages/464x323.png",
+          "../assets/productImages/464x323.png",
+        ],
+        large: [
+          "../assets/productImages/624x415.png",
+          "../assets/productImages/624x415.png",
+          "../assets/productImages/624x415.png",
+        ],
+      },
+    },
+  },
+  {
+    name: "Starry Stones",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    category: "ring",
+    price: 10000,
+    imageUrls: {
+      productImages: {
+        small: [
+          "../assets/productImages/81x57.png",
+          "../assets/productImages/81x57.png",
+          "../assets/productImages/81x57.png",
+        ],
+        medium: [
+          "../assets/productImages/464x323.png",
+          "../assets/productImages/464x323.png",
+          "../assets/productImages/464x323.png",
+        ],
+        large: [
+          "../assets/productImages/624x415.png",
+          "../assets/productImages/624x415.png",
+          "../assets/productImages/624x415.png",
+        ],
+      },
+      modelImages: {
+        small: [
+          "../assets/productImages/81x57.png",
+          "../assets/productImages/81x57.png",
+          "../assets/productImages/81x57.png",
+        ],
+        medium: [
+          "../assets/productImages/464x323.png",
+          "../assets/productImages/464x323.png",
+          "../assets/productImages/464x323.png",
+        ],
+        large: [
+          "../assets/productImages/624x415.png",
+          "../assets/productImages/624x415.png",
+          "../assets/productImages/624x415.png",
+        ],
+      },
+    },
+  },
+  {
+    name: "Cracking Gold",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    category: "ring",
+    price: 31000,
+    imageUrls: {
+      productImages: {
+        small: [
+          "../assets/productImages/81x57.png",
+          "../assets/productImages/81x57.png",
+          "../assets/productImages/81x57.png",
+        ],
+        medium: [
+          "../assets/productImages/464x323.png",
+          "../assets/productImages/464x323.png",
+          "../assets/productImages/464x323.png",
+        ],
+        large: [
+          "../assets/productImages/624x415.png",
+          "../assets/productImages/624x415.png",
+          "../assets/productImages/624x415.png",
+        ],
+      },
+      modelImages: {
+        small: [
+          "../assets/productImages/81x57.png",
+          "../assets/productImages/81x57.png",
+          "../assets/productImages/81x57.png",
+        ],
+        medium: [
+          "../assets/productImages/464x323.png",
+          "../assets/productImages/464x323.png",
+          "../assets/productImages/464x323.png",
+        ],
+        large: [
+          "../assets/productImages/624x415.png",
+          "../assets/productImages/624x415.png",
+          "../assets/productImages/624x415.png",
+        ],
+      },
+    },
+  },
+  {
+    name: "Cracking Silver",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    category: "ring",
+    price: 20000,
+    imageUrls: {
+      productImages: {
+        small: [
+          "../assets/productImages/81x57.png",
+          "../assets/productImages/81x57.png",
+          "../assets/productImages/81x57.png",
+        ],
+        medium: [
+          "../assets/productImages/464x323.png",
+          "../assets/productImages/464x323.png",
+          "../assets/productImages/464x323.png",
+        ],
+        large: [
+          "../assets/productImages/624x415.png",
+          "../assets/productImages/624x415.png",
+          "../assets/productImages/624x415.png",
+        ],
+      },
+      modelImages: {
+        small: [
+          "../assets/productImages/81x57.png",
+          "../assets/productImages/81x57.png",
+          "../assets/productImages/81x57.png",
+        ],
+        medium: [
+          "../assets/productImages/464x323.png",
+          "../assets/productImages/464x323.png",
+          "../assets/productImages/464x323.png",
+        ],
+        large: [
+          "../assets/productImages/624x415.png",
+          "../assets/productImages/624x415.png",
+          "../assets/productImages/624x415.png",
+        ],
+      },
+    },
+  },
+  {
+    name: "Starry Embers",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    category: "ring",
+    price: 3000,
+    imageUrls: {
+      productImages: {
+        small: [
+          "../assets/productImages/81x57.png",
+          "../assets/productImages/81x57.png",
+          "../assets/productImages/81x57.png",
+        ],
+        medium: [
+          "../assets/productImages/464x323.png",
+          "../assets/productImages/464x323.png",
+          "../assets/productImages/464x323.png",
+        ],
+        large: [
+          "../assets/productImages/624x415.png",
+          "../assets/productImages/624x415.png",
+          "../assets/productImages/624x415.png",
+        ],
+      },
+      modelImages: {
+        small: [
+          "../assets/productImages/81x57.png",
+          "../assets/productImages/81x57.png",
+          "../assets/productImages/81x57.png",
+        ],
+        medium: [
+          "../assets/productImages/464x323.png",
+          "../assets/productImages/464x323.png",
+          "../assets/productImages/464x323.png",
+        ],
+        large: [
+          "../assets/productImages/624x415.png",
+          "../assets/productImages/624x415.png",
+          "../assets/productImages/624x415.png",
+        ],
+      },
+    },
+  },
+  {
+    name: "Black Chained Gold",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    category: "earring",
     price: 5000,
     imageUrls: {
       productImages: {
@@ -42,10 +344,11 @@ exports.mockProductData = [
     },
   },
   {
-    name: "Necklace",
-    description: "This necklace is made of diamonds and gold",
-    category: "necklace",
-    price: 5000,
+    name: "Black Pearl",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    category: "bracelet",
+    price: 7000,
     imageUrls: {
       productImages: {
         small: [
