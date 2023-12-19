0f293b32cae4ae2e3f3bffb0af8d7e3f97b5bc32
maxtkarlsson <113439377+maxtkarlsson@users.noreply.github.com>
2023-12-19 10:23:57 +0100

    (feat): Add get product by id functionality

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,7 +1,32 @@
 const Product = require("../models/Product");
 
 exports.getAllProducts = async (req, res) => {
-  const products = await Product.find();
+  const limit = Number(req.query?.limit || 20);
 
-  return res.json(products);
+  const offset = Number(req.query?.offset || 0);
+
+  const products = await Product.find().limit(limit).skip(offset);
+
+  const totalProductsInDatabase = await Product.countDocuments();
+
+  return res.json({
+    data: products,
+    meta: {
+      total: totalProductsInDatabase,
+      limit: limit,
+      offset: offset,
+      count: products.length,
+    },
+  });
+};
+
+exports.getProductById = async (req, res) => {
+  const productId = req.params.productId;
+
+  const product = await Product.findById(productId);
+
+  if (!product)
+    throw new NotFoundError("A product with that ID does not exist");
+
+  return res.status(200).json(product);
 };
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const { getAllProducts } = require("../controllers/productController");
+const {
+  getAllProducts,
+  getProductById,
+} = require("../controllers/productController");
 const router = express.Router();
 
 // GET - /api/v1/products
 router.get("/", getAllProducts);
+
+// GET - /api/v1/products/:productId
+router.get("/:productId", getProductById);
 
 module.exports = router;
