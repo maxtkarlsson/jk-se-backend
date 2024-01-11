1df156d535ca0e2135c1ea69a44e0a4e7c4b88de
maxtkarlsson <113439377+maxtkarlsson@users.noreply.github.com>
2024-01-11 12:34:08 +0100

    (fix): Add more products to seed file

--- seedDb/products.js
+++ seedDb/products.js
@@ -224,4 +224,254 @@ exports.mockProductData = [
       },
     },
   },
+  {
+    name: "Black Stoney",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    category: "ring",
+    price: 3000,
+    imageUrls: {
+      productImages: {
+        small: [
+          "https://raw.githubusercontent.com/maxtkarlsson/jk-se/main/public/productImages/100x67/jennyprodukt26maj2016nr186-100x67.webp",
+        ],
+        medium: [
+          "https://raw.githubusercontent.com/maxtkarlsson/jk-se/main/public/productImages/464x348/jennyprodukt26maj2016nr186-464x348.webp",
+        ],
+        large: [
+          "https://raw.githubusercontent.com/maxtkarlsson/jk-se/main/public/productImages/1000x667/jennyprodukt26maj2016nr186-1000x667.webp",
+        ],
+      },
+      modelImages: {
+        small: ["../assets/productImages/81x57.png"],
+        medium: ["../assets/productImages/464x323.png"],
+        large: ["../assets/productImages/624x415.png"],
+      },
+    },
+  },
+  {
+    name: "Golden Copper Rose",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    category: "ring",
+    price: 15000,
+    imageUrls: {
+      productImages: {
+        small: [
+          "https://raw.githubusercontent.com/maxtkarlsson/jk-se/main/public/productImages/100x67/jennyprodukt26maj2016nr307-100x67.webp",
+        ],
+        medium: [
+          "https://raw.githubusercontent.com/maxtkarlsson/jk-se/main/public/productImages/464x348/jennyprodukt26maj2016nr307-464x348.webp",
+        ],
+        large: [
+          "https://raw.githubusercontent.com/maxtkarlsson/jk-se/main/public/productImages/1000x667/jennyprodukt26maj2016nr307-1000x667.webp",
+        ],
+      },
+      modelImages: {
+        small: ["../assets/productImages/81x57.png"],
+        medium: ["../assets/productImages/464x323.png"],
+        large: ["../assets/productImages/624x415.png"],
+      },
+    },
+  },
+  {
+    name: "Chained Stick",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    category: "necklace",
+    price: 13000,
+    imageUrls: {
+      productImages: {
+        small: [
+          "https://raw.githubusercontent.com/maxtkarlsson/jk-se/main/public/productImages/100x67/jennyprodukt26maj2016nr441-100x67.webp",
+        ],
+        medium: [
+          "https://raw.githubusercontent.com/maxtkarlsson/jk-se/main/public/productImages/464x348/jennyprodukt26maj2016nr441-464x348.webp",
+        ],
+        large: [
+          "https://raw.githubusercontent.com/maxtkarlsson/jk-se/main/public/productImages/1000x667/jennyprodukt26maj2016nr441-1000x667.webp",
+        ],
+      },
+      modelImages: {
+        small: ["../assets/productImages/81x57.png"],
+        medium: ["../assets/productImages/464x323.png"],
+        large: ["../assets/productImages/624x415.png"],
+      },
+    },
+  },
+  {
+    name: "Black Chained Stick",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    category: "necklace",
+    price: 19000,
+    imageUrls: {
+      productImages: {
+        small: [
+          "https://raw.githubusercontent.com/maxtkarlsson/jk-se/main/public/productImages/100x67/jennyprodukt26maj2016nr360-100x67.webp",
+        ],
+        medium: [
+          "https://raw.githubusercontent.com/maxtkarlsson/jk-se/main/public/productImages/464x348/jennyprodukt26maj2016nr360-464x348.webp",
+        ],
+        large: [
+          "https://raw.githubusercontent.com/maxtkarlsson/jk-se/main/public/productImages/1000x667/jennyprodukt26maj2016nr360-1000x667.webp",
+        ],
+      },
+      modelImages: {
+        small: ["../assets/productImages/81x57.png"],
+        medium: ["../assets/productImages/464x323.png"],
+        large: ["../assets/productImages/624x415.png"],
+      },
+    },
+  },
+  {
+    name: "Hoops of Rings",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    category: "bracelet",
+    price: 5000,
+    imageUrls: {
+      productImages: {
+        small: [
+          "https://raw.githubusercontent.com/maxtkarlsson/jk-se/main/public/productImages/100x67/jennyprodukt26maj2016nr089-100x67.webp",
+        ],
+        medium: [
+          "https://raw.githubusercontent.com/maxtkarlsson/jk-se/main/public/productImages/464x348/jennyprodukt26maj2016nr089-464x348.webp",
+        ],
+        large: [
+          "https://raw.githubusercontent.com/maxtkarlsson/jk-se/main/public/productImages/1000x667/jennyprodukt26maj2016nr089-1000x667.webp",
+        ],
+      },
+      modelImages: {
+        small: ["../assets/productImages/81x57.png"],
+        medium: ["../assets/productImages/464x323.png"],
+        large: ["../assets/productImages/624x415.png"],
+      },
+    },
+  },
+  {
+    name: "Silver Bubbles",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    category: "necklace",
+    price: 16000,
+    imageUrls: {
+      productImages: {
+        small: [
+          "https://raw.githubusercontent.com/maxtkarlsson/jk-se/main/public/productImages/100x67/jennyprodukt26maj2016nr547-100x67.webp",
+        ],
+        medium: [
+          "https://raw.githubusercontent.com/maxtkarlsson/jk-se/main/public/productImages/464x348/jennyprodukt26maj2016nr547-464x348.webp",
+        ],
+        large: [
+          "https://raw.githubusercontent.com/maxtkarlsson/jk-se/main/public/productImages/1000x667/jennyprodukt26maj2016nr547-1000x667.webp",
+        ],
+      },
+      modelImages: {
+        small: ["../assets/productImages/81x57.png"],
+        medium: ["../assets/productImages/464x323.png"],
+        large: ["../assets/productImages/624x415.png"],
+      },
+    },
+  },
+  {
+    name: "Three Bowls of Metal",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    category: "necklace",
+    price: 25000,
+    imageUrls: {
+      productImages: {
+        small: [
+          "https://raw.githubusercontent.com/maxtkarlsson/jk-se/main/public/productImages/100x67/jennyprodukt26maj2016nr524-100x67.webp",
+        ],
+        medium: [
+          "https://raw.githubusercontent.com/maxtkarlsson/jk-se/main/public/productImages/464x348/jennyprodukt26maj2016nr524-464x348.webp",
+        ],
+        large: [
+          "https://raw.githubusercontent.com/maxtkarlsson/jk-se/main/public/productImages/1000x667/jennyprodukt26maj2016nr524-1000x667.webp",
+        ],
+      },
+      modelImages: {
+        small: ["../assets/productImages/81x57.png"],
+        medium: ["../assets/productImages/464x323.png"],
+        large: ["../assets/productImages/624x415.png"],
+      },
+    },
+  },
+  {
+    name: "Turquoise",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    category: "ring",
+    price: 9000,
+    imageUrls: {
+      productImages: {
+        small: [
+          "https://raw.githubusercontent.com/maxtkarlsson/jk-se/main/public/productImages/100x67/jennyprodukt26maj2016nr311-100x67.webp",
+        ],
+        medium: [
+          "https://raw.githubusercontent.com/maxtkarlsson/jk-se/main/public/productImages/464x348/jennyprodukt26maj2016nr311-464x348.webp",
+        ],
+        large: [
+          "https://raw.githubusercontent.com/maxtkarlsson/jk-se/main/public/productImages/1000x667/jennyprodukt26maj2016nr311-1000x667.webp",
+        ],
+      },
+      modelImages: {
+        small: ["../assets/productImages/81x57.png"],
+        medium: ["../assets/productImages/464x323.png"],
+        large: ["../assets/productImages/624x415.png"],
+      },
+    },
+  },
+  {
+    name: "White",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    category: "ring",
+    price: 5000,
+    imageUrls: {
+      productImages: {
+        small: [
+          "https://raw.githubusercontent.com/maxtkarlsson/jk-se/main/public/productImages/100x67/jennyprodukt26maj2016nr251-100x67.webp",
+        ],
+        medium: [
+          "https://raw.githubusercontent.com/maxtkarlsson/jk-se/main/public/productImages/464x348/jennyprodukt26maj2016nr251-464x348.webp",
+        ],
+        large: [
+          "https://raw.githubusercontent.com/maxtkarlsson/jk-se/main/public/productImages/1000x667/jennyprodukt26maj2016nr251-1000x667.webp",
+        ],
+      },
+      modelImages: {
+        small: ["../assets/productImages/81x57.png"],
+        medium: ["../assets/productImages/464x323.png"],
+        large: ["../assets/productImages/624x415.png"],
+      },
+    },
+  },
+  {
+    name: "Grey",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    category: "ring",
+    price: 8000,
+    imageUrls: {
+      productImages: {
+        small: [
+          "https://raw.githubusercontent.com/maxtkarlsson/jk-se/main/public/productImages/100x67/jennyprodukt26maj2016nr254-100x67.webp",
+        ],
+        medium: [
+          "https://raw.githubusercontent.com/maxtkarlsson/jk-se/main/public/productImages/464x348/jennyprodukt26maj2016nr254-464x348.webp",
+        ],
+        large: [
+          "https://raw.githubusercontent.com/maxtkarlsson/jk-se/main/public/productImages/1000x667/jennyprodukt26maj2016nr254-1000x667.webp",
+        ],
+      },
+      modelImages: {
+        small: ["../assets/productImages/81x57.png"],
+        medium: ["../assets/productImages/464x323.png"],
+        large: ["../assets/productImages/624x415.png"],
+      },
+    },
+  },
 ];
--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -5,7 +5,7 @@ exports.getAllProducts = async (req, res) => {
   try {
     const page = Number(req.query?.page || 1);
 
-    const pageSize = Number(req.query?.limit || 6);
+    const pageSize = Number(req.query?.limit || 10);
 
     const skip = (page - 1) * pageSize;
 
